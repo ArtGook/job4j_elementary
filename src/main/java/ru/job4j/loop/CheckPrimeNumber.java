9ba2b0dc84243bd975b16f843d3f201087ad281d
ArtGook <artemgabrusenok305@gmail.com>
2023-02-09 16:19:46 +0300

    5.6. Простые числа [#156314 #66461]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean result = true;
+        if (number <= 1) {
+            result = false;
+        }
+            for (int i = 2; i < number - 1; i++) {
+                if ((number % i) == 0) {
+                    result = false;
+                    break;
+                }
+            }
+        return result;
+    }
+}

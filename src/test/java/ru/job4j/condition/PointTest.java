3d560f1d17f049e93494465f0134cb76ef58b916
ArtGook <artemgabrusenok305@gmail.com>
2023-03-18 17:01:19 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -9,7 +9,6 @@ public class PointTest {
     public void when09to02then2() {
         Point distance = new Point(0, 9);
         Point that = new Point(0, 2);
-        distance.distance(that);
         double expected = 7.0;
         assertThat(distance.distance(that)).isEqualTo(expected);
     }
@@ -18,7 +17,6 @@ public class PointTest {
     public void when19to13then2() {
         Point distance = new Point(1, 9);
         Point that = new Point(1, 3);
-        distance.distance(that);
         double expected = 6.0;
         assertThat(distance.distance(that)).isEqualTo(expected);
     }
@@ -27,7 +25,6 @@ public class PointTest {
     public void when50to10then2() {
         Point distance = new Point(5, 0);
         Point that = new Point(1, 0);
-        distance.distance(that);
         double expected = 4.0;
         assertThat(distance.distance(that)).isEqualTo(expected);
     }
